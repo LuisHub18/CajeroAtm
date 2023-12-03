a2bac9d6549cdacc7d14b56549c45f06a776948b
Daniel Castro <l20170627@culiacan.tecnm.mx>
2023-12-03 04:11:39 -0700

    actualizacion del proyecto

--- src/Entidades/Cuenta.java
+++ src/Entidades/Cuenta.java
@@ -1,19 +1,57 @@
 package Entidades;
 
 import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cuenta {
-    private String num_cuenta;
     private int nip;
     private UUID cliente;
     private double saldo;
 
-    public Cuenta(String num_cuenta, UUID cliente, int nip, double saldo) {
-        this.num_cuenta = num_cuenta;
+    public Map<String, Cuenta> Cuentas = new HashMap<>();
+
+    public Cuenta(UUID cliente, int nip, double saldo) {
         this.cliente = cliente;
         this.nip = nip;
         this.saldo = saldo;
     }
+
+    public Cuenta() {
+    }
+
+    public void agregarCuenta(String num_cuenta, UUID cliente, int nip, double saldo){
+        Cuentas.put(num_cuenta, new Cuenta(cliente,nip,saldo));
+    }
+    public boolean validarCuenta(String cuenta, int nip){
+        if (Cuentas.containsKey(cuenta) && obtenerNip(nip))
+            return true;
+        return false;
+    }
+
+    public String numCuenta (){
+        for (Map.Entry<String, Cuenta> entry : Cuentas.entrySet()) {
+            String key = entry.getKey();
+            return key;
+        }
+        return "";
+    }
+
+    public boolean obtenerNip(int nip) {
+        for (Map.Entry<String, Cuenta> entry : Cuentas.entrySet()) {
+            Cuenta cuenta = entry.getValue();
+            if (cuenta.getNip() == nip)
+                 return true;
+        }
+
+        return false;
+    }
+
+
+    public Cuenta buscarCuenta(String cuenta){
+            return Cuentas.get(cuenta);
+    }
+
 
     public int getNip() {
         return this.nip;
@@ -24,8 +62,5 @@ public class Cuenta {
     }
     public double getSaldo() {
         return this.saldo;
-    }
-    public String getNumCuenta() {
-        return this.num_cuenta;
     }
 }
--- src/Transferencia.java
+++ src/Transferencia.java
@@ -4,17 +4,18 @@ import java.time.LocalDate;
 
 public class Transferencia extends Clase_Abstractaa {
 
+    private Cuenta cuenta = new Cuenta();
     private Cuenta cuentaOrigen;
     private Cuenta cuentaDestino;
 
-    public Transferencia(Cuenta cuentaOrigen, Cuenta cuentaDestino) {
+    public Transferencia(Cuenta cuentaOrigen, String destino) {
         this.cuentaOrigen = cuentaOrigen;
-        this.cuentaDestino = cuentaDestino;
+        cuentaDestino = cuenta.buscarCuenta(destino);
     }
     @Override
     public void Transacciones() {
     			System.out.print("Cuanto quieres Transferir: ");
-                Transferencia();
+                int transferencia = Transferencia();
                 if (transferencia <= cuentaOrigen.getSaldo()) {
                     cuentaOrigen.setSaldo(cuentaOrigen.getSaldo() - transferencia);
                     cuentaDestino.setSaldo(cuentaDestino.getSaldo() + transferencia);
@@ -23,7 +24,7 @@ public class Transferencia extends Clase_Abstractaa {
                     System.out.println("=================BBVA===============");
                     System.out.println("				Debito				");
                     System.out.println("Se realizo una transferencia el " + LocalDate.now());
-                    System.out.println("A la cuenta "+ cuentaOrigen.getNumCuenta());
+                    System.out.println("A la cuenta "+ cuentaOrigen.numCuenta());
                     System.out.println("transferiste : " + transferencia);
                     System.out.println("Tu saldo actual es: " + cuentaOrigen.getSaldo());
                     System.out.println("====================================");
